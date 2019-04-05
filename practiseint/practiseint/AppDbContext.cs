d56567d88c73970eb141fbafdbf925ea7748e5b5
Henil Patel <hpatel@alk.com>
2019-04-05 15:12:02 -0400

    CRUD

---
+++ practiseint/practiseint/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace practiseint
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+   
+    }
+}
---
+++ practiseint/practiseint/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace practiseint
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+
+        [Required , StringLength(100)]
+        public string  Name { get; set; }
+    }
+}
